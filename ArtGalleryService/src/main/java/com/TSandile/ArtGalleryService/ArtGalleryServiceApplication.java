420734daa2358bab26c2f864b209980620bf33d4
TSandile <Sandile.Tshabalala11@gmail.com>
2025-08-15 01:05:14 +0200

    Initial web service, using Spring framework

---
+++ ArtGalleryService/src/main/java/com/TSandile/ArtGalleryService/ArtGalleryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.TSandile.ArtGalleryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtGalleryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtGalleryServiceApplication.class, args);
+	}
+
+}
---
+++ ArtGalleryService/src/test/java/com/TSandile/ArtGalleryService/ArtGalleryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TSandile.ArtGalleryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtGalleryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
