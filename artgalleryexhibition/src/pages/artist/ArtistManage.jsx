1e127a80f5665c932dbc217e8e981a6fbecc5d53
TSandile <Sandile.Tshabalala11@gmail.com>
2025-09-10 15:59:01 +0200

    add packages and modified page

--- artgalleryexhibition/src/pages/artist/ArtistManage.jsx
+++ artgalleryexhibition/src/pages/artist/ArtistManage.jsx
@@ -1,44 +1,53 @@
 import { Form } from "react-bootstrap";
+import { Button, TextField } from "@mui/material";
 import "../artist/ArtistManage.css";
 
 const ArtistManage = () => {
   return (
     <>
       <div className="center-form d-flex justify-content-center align-items-center">
+        <h1>Add Artist</h1>
         <section className="form-section">
           <Form className="d-flex justify-content-around">
             <Form.Group className="mb-3" controlId="formGroupEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-          </Form>
-
-          <Form className="d-flex justify-content-around">
-            <Form.Group className="mb-3" controlId="formGroupEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <TextField
+                className="text-field"
+                id="outlined-basic"
+                label="Artist Name"
+                varient="outlined"
+                type="text"
+                sx={{ width: 340 }}
+              />
             </Form.Group>
           </Form>
 
           <Form className="d-flex justify-content-around">
             <Form.Group className="mb-3" controlId="formGroupEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <TextField
+                id="outlined-basic"
+                label="Biography"
+                varient="outlined"
+                type="text"
+                sx={{ width: 340 }}
+              />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+          </Form>
+
+          <Form className="d-flex justify-content-around">
+            <span> Upload Image</span>
+          </Form>
+
+          <Form className="d-flex justify-content-around">
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <TextField varient="outlined" type="file" />
             </Form.Group>
           </Form>
         </section>
+        <div>
+          <Button id="btn-confirm" variant="contained" size="large">
+            Confirm
+          </Button>
+        </div>
       </div>
     </>
   );
